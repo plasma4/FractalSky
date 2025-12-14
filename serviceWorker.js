55cef7270aa59e71d5bec79c258f9a3f2693e056
plasma4 <72562754+plasma4@users.noreply.github.com>
2025-12-13 23:25:44 -0600

    Try a header injection change

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,77 +1,94 @@
-const CACHE_NAME = "fractalsky"
+const CACHE_NAME = "fractalsky";
 const urlsToCache = [
-    "./",
-    "./index.html",
-    "./LICENSE",
-    "./main.js",
-    "./worker.js",
-    "./manifest.json",
-    "./fractal.wasm",
-    "./fractalUnshared.wasm"
-]
+  "./",
+  "./index.html",
+  "./main.js",
+  "./worker.js",
+  "./manifest.json",
+  "./fractal.wasm",
+  "./fractalUnshared.wasm",
+];
 
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log("Service Worker: Caching app shell")
-                return cache.addAll(urlsToCache)
-            })
-            .then(() => {
-                console.log("Service Worker: Forcing immediate activation (skipWaiting).")
-                return self.skipWaiting()
-            })
-    )
-})
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => {
+        console.log("SW: Caching app shell");
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => {
+        console.log("SW: Forcing immediate activation (skipWaiting).");
+        return self.skipWaiting();
+      })
+  );
+});
 
-self.addEventListener("activate", e => {
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== CACHE_NAME) {
-                        console.log("Service Worker: Deleting old cache:", cache)
-                        return caches.delete(cache)
-                    }
-                })
-            )
-        }).then(() => {
-            console.log("Service Worker: Claiming clients.")
-            return self.clients.claim()
-        })
-    )
-})
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cache) => {
+            if (cache !== CACHE_NAME) {
+              console.log("SW: Deleting old cache:", cache);
+              return caches.delete(cache);
+            }
+          })
+        );
+      })
+      .then(() => {
+        console.log("SW: Claiming clients.");
+        return self.clients.claim();
+      })
+  );
+});
 
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") {
+    return;
+  }
 
-self.addEventListener("fetch", e => {
-    if (e.request.method !== "GET") {
-        return
-    }
+  if (!e.request.url.startsWith(self.location.origin)) {
+    // Return original fetch for cross-origin requests
+    return e.respondWith(fetch(e.request));
+  }
 
-    // Cross-origin request
-    if (!e.request.url.startsWith(self.location.origin)) {
-        return e.respondWith(fetch(e.request))
-    }
+  e.respondWith(
+    fetch(e.request)
+      .then(async (networkResponse) => {
+        if (networkResponse.status !== 200) {
+          return networkResponse;
+        }
 
-    e.respondWith(
-        fetch(e.request)
-            .then(async (networkResponse) => {
-                const responseClone = networkResponse.clone()
-                const cache = await caches.open(CACHE_NAME)
-                if (networkResponse.status === 200) {
-                    cache.put(e.request, responseClone)
-                }
+        const cacheResponse = networkResponse.clone();
+        const bodyResponse = networkResponse.clone();
 
-                return networkResponse
-            })
-            .catch(async (err) => {
-                const cachedResponse = await caches.match(e.request)
-                if (cachedResponse) {
-                    return cachedResponse
-                }
+        // Caching step
+        const cache = await caches.open(CACHE_NAME);
+        cache.put(e.request, cacheResponse);
 
-                console.error(err)
-                return Response.error()
-            })
-    )
-})
+        const newHeaders = new Headers(bodyResponse.headers);
+        newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+        newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+
+        const modifiedResponse = new Response(bodyResponse.body, {
+          status: bodyResponse.status,
+          statusText: bodyResponse.statusText,
+          headers: newHeaders,
+        });
+
+        return modifiedResponse;
+      })
+      .catch(async (err) => {
+        // Network failed (offline scenario)
+        const cachedResponse = await caches.match(e.request);
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return Response.error();
+      })
+  );
+});
